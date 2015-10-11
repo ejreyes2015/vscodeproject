838660419ca9e378add04a3bf14f4d85178bcf5f
ejreyes2015 <editjreyes@gmail.com>
2015-10-11 09:16:33 -0500

    done

---
+++ ChallengeRadioButton/ChallengeRadioButton/default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengeRadioButton._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        Your note taking preferences:<br />
+        <br />
+        <br />
+        <asp:RadioButton ID="pencilRadioButton" runat="server" GroupName="utensil" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="penRadioButton" runat="server" GroupName="utensil" Text="Pen" />
+    
+    </div>
+        <asp:RadioButton ID="phoneRadioButton" runat="server" GroupName="utensil" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="tabletRadioButton" runat="server" GroupName="utensil" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Image ID="resultImage" runat="server" BorderStyle="None" Height="185px" Width="150px" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ChallengeRadioButton/ChallengeRadioButton/default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeRadioButton
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (pencilRadioButton.Checked)
+            {
+                resultLabel.Text = "Way to be old school graphite";
+                resultImage.ImageUrl = "Images/pencil.png";
+            }
+            else if (penRadioButton.Checked)
+            {
+                resultLabel.Text = "Way to be old school ink";
+                resultImage.ImageUrl = "Images/pen.png";
+            }
+            else if (phoneRadioButton.Checked)
+            {
+                resultLabel.Text = "How do you take notes on your phone?";
+                resultImage.ImageUrl = "Images/phone.png";
+            }
+            else if (tabletRadioButton.Checked)
+            {
+                resultLabel.Text = "Do you have a keyboard for your tablet?";
+                resultImage.ImageUrl = "Images/tablet.png";
+            }
+            else
+            {
+                resultLabel.Text = " You really need to make a choice";
+            }
+        }
+    }
+}
